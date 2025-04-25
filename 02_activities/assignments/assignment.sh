fec0b460710b2ef797019b9be08d86a6265b1872
Mahi <mahic.1123@gmail.com>
2025-04-25 15:30:06 -0400

    Update based on error

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,8 +38,10 @@ rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -R ./data/processed/* > ./data/inventory.txtgit 
+ls -R ./data/processed/* > ./data/inventory.txt 
+
 
 ###########################################
 
 echo "Project setup is complete!"
+# 
